54feed86e81081a9b46973100d57ab5f36cac504
Jhanluy <53126542+Jhanluy@users.noreply.github.com>
2019-11-03 13:53:10 -0500

    Revert "cambioss3"
    This reverts commit de8f1336916e722adf11446cf01560d1a3b3edbc.

---
+++ Codigo Proyecto/Sanambiente/cliente/src/app/Components/Body/Organizations_Body/Organizations_Body.component.ts
@@ -0,0 +1,70 @@
+/* Clase que contiene los metodos y la logica de la vista html en la cual se crean y editan las organizaciones*/
+
+/* Se importan los componentes y caracteristicas necesarias para el funcionamiento de esta clase */
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { OrganizationsService } from '../../../Services/Organizations_Service/Organizations_Service';
+
+@Component({
+  selector: 'app-Organizations_Body',
+  templateUrl: './Organizations_Body.component.html'
+})
+export class OrganizationsBodyComponent implements OnInit {
+
+  public formOrganization: FormGroup;
+  public edit: boolean = false;
+  public arrayOrganizations;
+  public lengthOrganizations: number;
+  public hide = false;
+
+  constructor(private organizationService: OrganizationsService, private router: Router, private activedRoute: ActivatedRoute) {
+    this.formOrganization = new FormGroup({
+      'nombre_organizacion': new FormControl('', [Validators.required, Validators.maxLength(49.9)]),
+      'observacion_organizacion': new FormControl('', [Validators.required, Validators.maxLength(49.9)]),
+    });
+    this.arrayOrganizations = {};
+
+  }
+
+  /* Se establecen los metodos que se ejecutaran cada vez que se visite la vista Organizations_Body */
+  ngOnInit(): void {
+    this.viewDataById() // Toma el id de la organizacion, cuando se vaya a editar alguna de ellas
+  }
+
+  @HostBinding('class') classes = 'row';  // Genera que las columnas de ordenamiento del contenido en la vista html esten alineadas.
+
+
+  /* Método con el cual se crea una nueva organizacion */
+  async createOrganization() {
+    if (this.formOrganization.valid) {
+      await this.organizationService.createOrganization(this.formOrganization.value);
+      alert('Organización creada correctamente');
+      this.router.navigate(['/organization']);
+    }
+  }
+
+  /* Método con el cual se identifica la organizacion cuya información va a ser actualizada */
+  async viewDataById() {
+    let id = this.activedRoute.snapshot.params.id_organizacion;
+    if (id !== undefined) {
+      let organization = await this.organizationService.viewOrganizationById(id).subscribe((element: any) => {
+        this.arrayOrganizations = element.message[0];
+        this.edit = true;
+        this.hide = true;
+      });
+    }
+  }
+
+  /* Método con el cual se actualiza la información de la organizacion seleccionada */
+  async updateOrganization() {
+    let id = this.activedRoute.snapshot.params.id_organizacion;
+    this.formOrganization.patchValue({
+      nombre_organizacion: this.arrayOrganizations.nombre_organizacion,
+      observacion_organizacion: this.arrayOrganizations.observacion_organizacion
+    })
+    this.organizationService.updateOrganization(this.formOrganization.value, id)
+    alert('Organización actualizada correctamente');
+    this.router.navigate(['/organization']);
+  }
+}
---
+++ Codigo Proyecto/Sanambiente/servidor/src/Components/Stations_Component/Stations_Route.ts
@@ -0,0 +1,29 @@
+/*Esta clase contiene la configuracion de las rutas del lado del servidor
+ utilizadas para la tabla estaciones */
+
+import { Router } from 'express';
+import Stations_Service from './Stations_Service'; // Se importa la constante StationsService de la clase Station_Service
+import stationService from './Stations_Service';
+
+
+class StationsRoute {
+
+    public router: Router = Router();
+
+    constructor() {
+        this.configuracion();
+    }
+    /* Se establecen los metodos de la clase Stations_Service, que seran utilizados cuando se llame a cada una de 
+    las rutas de estaciones*/
+
+    configuracion(): void {
+        this.router.post('/createStation', stationService.create);
+        this.router.get('/viewStationById/:id_estacion', stationService.viewById);
+        this.router.get('/viewStations', stationService.view);
+        this.router.put('/updateStations/:id_estacion', stationService.update);
+    }
+}
+
+// Se crea y exporta una constante que contiene las rutas de esta clase.
+const stationsRoute = new StationsRoute();
+export default stationsRoute.router; 
