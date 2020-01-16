d57e78c39c0c0385fecf31cf9dd4a9e6c03af521
jaw0094 <jaw0094@hotmail.com>
2020-01-16 17:29:36 -0500

    ...

---
+++ Codigo Proyecto/Sanambiente/cliente/src/app/Components/Body/Variables_List/Variables_List.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Variables_List',
+  templateUrl: './Variables_List.component.html',
+  styleUrls: ['./Variables_List.component.css']
+})
+export class VariablesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Codigo Proyecto/Sanambiente/cliente/src/app/Services/Variables_Service/Variables_Service.ts
@@ -0,0 +1,35 @@
+/* Clase que contiene los servicios del lado del cliente para la tabla variable */
+import { Injectable } from '@angular/core';
+import { Variable } from '../../Models/Variables_Model/Variable'; // Se importa el modelo Variable para Variable
+import BaseService from '../Base_Service/Base_Service'; // Se importan las propiedades de la clase Base_Service
+import { environment } from 'src/environments/environment'; // Se importan las constantes de la clase environment
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VariablesService {
+
+  constructor(private baseService: BaseService) {}
+
+  /* Se crean los metodos utilizados por la vista de Variable */
+  createVariable (body: Variable) {
+    return this.baseService.create(body, `${environment.hostCreateVariable}`);
+    
+  }
+
+  viewVariables() {
+    return this.baseService.view(`${environment.viewVariables}`).toPromise().then((data: any) => data.message).catch((error) => error);
+  }
+
+  updateVariable(body: Variable, id_variable: number) {
+    return this.baseService.update(body, `${environment.hostUpdateVariable}/${id_variable}`);
+  }
+
+  viewVariableById(id_variable: number) {
+    return this.baseService.view(`${environment.hostviewVariableById}/${id_variable}`);
+  }
+
+}
+
+}
