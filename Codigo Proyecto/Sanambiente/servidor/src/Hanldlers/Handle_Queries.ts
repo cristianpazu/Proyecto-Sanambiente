5119eba1e656188cad6568ae5b6c7e1bd67ab16f
jaw0094 <jaw0094@hotmail.com>
2020-03-04 22:01:49 -0500

    correccion de error actualizar alerta

--- Codigo Proyecto/Sanambiente/servidor/src/Hanldlers/Handle_Queries.ts
+++ Codigo Proyecto/Sanambiente/servidor/src/Hanldlers/Handle_Queries.ts
@@ -28,7 +28,7 @@ export const handlerQuery = {
     createAlert: 'INSERT INTO alertas (nombre_alerta, email_alerta, contrasena_alerta, servidorsmtp_alerta, puertosmtp_alerta, seguridad_alerta, autenticacion_alerta, emailpara_alerta, asunto_alerta, emailde_alerta, observacion_alerta) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)',
     viewAlerts: 'SELECT * FROM alertas order by id_alerta',
     viewAlert: 'SELECT * FROM alertas where id_alerta=$1',
-    updateAlert: 'UPDATE alertas set nombre_alerta=$1, email_alerta=$2, contrasena_alerta=$3, servidorsmtp_alerta=$4, puertosmtp_alerta=$5, seguridad_alerta=$6, autenticacion_alerta=$7, emailpara_alerta=$8, asunto_alerta,=$9  emailde_alerta=$10, observacion_alerta=$11 where id_alerta=$12',
+    updateAlert: 'UPDATE alertas set nombre_alerta=$1, email_alerta=$2, contrasena_alerta=$3, servidorsmtp_alerta=$4, puertosmtp_alerta=$5, seguridad_alerta=$6, autenticacion_alerta=$7, emailpara_alerta=$8, asunto_alerta=$9,  emailde_alerta=$10, observacion_alerta=$11 where id_alerta=$12',
     // organizacion
     createOrganizacion: 'INSERT INTO organizaciones (nombre_organizacion, observacion_organizacion, email_organizacion, telefono_organizacion) VALUES ($1,$2,$3,$4)',
     viewOrganizations: 'SELECT * FROM organizaciones order by id_organizacion',
